e817f53cf903a3224809be6cc5756b01df5f8c25
Brajesh Kumar <brajesh.kumar3@globallogic.com>
2026-06-07 23:54:24 +0530

    Add SSL for Render PostgreSQL

--- notes-backend/src/server.js
+++ notes-backend/src/server.js
@@ -33,7 +33,7 @@ app.get("/api/profile", authMiddleware, (req, res) => {
     });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
